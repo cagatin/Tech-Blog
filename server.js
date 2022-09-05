af86837b3aff9272b9693bf10282732d2af00c60
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-05 10:24:19 -0700

    Created basic server file to import dependencies and create server

---
+++ server.js
@@ -0,0 +1,26 @@
+//Import Dependencies
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// create handlebars.js engine object
+const hbs = exphbs.create({});
+
+// register the view enging
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// middlewear
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// todo: set up middlewear for session, routes, etc.
+
+// Create Server
+app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
