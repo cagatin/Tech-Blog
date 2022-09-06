85440497dfc59fab8cc87ad92cc4e27dfa0a1f2a
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-06 10:19:47 -0700

    Added basic handlebars structure

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const homeRoutes = require('../controller/homeRoutes');
-const apiRoutes = require('../controller/api/apiRoutes');
+const homeRoutes = require('../controller/homeRoutes.js');
+const apiRoutes = require('../controller/api/apiRoutes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
