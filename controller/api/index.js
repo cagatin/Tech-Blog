c8f98d0e66c9174d92be667f26737fab9a811c77
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-06 10:11:09 -0700

    Moved index.js content of controller/api into index.js of routes

--- controller/api/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const apiRoutes = require('../controller/api/apiRoutes');
-
-router.use('/api', apiRoutes);
-
-module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const homeRoutes = require('../controller/homeRoutes');
+const apiRoutes = require('../controller/api/apiRoutes');
 
 router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
