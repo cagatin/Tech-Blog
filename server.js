90a4e2f8c4e18f48fb53b01f34a186f5220a202e
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-06 11:22:18 -0700

    Added sessions to server file

--- server.js
+++ server.js
@@ -5,12 +5,26 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 const sequelize = require('./config/connection');
 const routes = require('./routes/index');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // create handlebars.js engine object
 const hbs = exphbs.create({});
+
+// create session
+const sesh = {
+    secret: 'placeholder secret',
+    cookie: {},
+    resave: false,
+    saveUninitiated: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sesh));
 
 // register the view enging
 app.engine('handlebars', hbs.engine);
