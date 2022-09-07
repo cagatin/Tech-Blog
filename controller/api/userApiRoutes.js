733039747fb5b5facd4dde4900a5470e02cde1d6
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-07 09:09:47 -0700

    Added index.js file for relationships between models

--- controller/api/userApiRoutes.js
+++ controller/api/userApiRoutes.js
@@ -5,5 +5,17 @@ router.post('/login', (req, res) => {
     res.json({ message: 'test' });
 })
 
+/*
+43:57 in lecture video
+to log someone in, use
+req.session.save(()=> {
+    req.session.loggedIn = true;
+
+    res
+        .status(200)
+        .json({user: dbUserData, //message here})
+})
+*/
+
 
 module.exports = router;
