aac1cf508b5610db57b54de206d0ae9a512cefeb
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-07 09:59:20 -0700

    Created api route for user to log into the tech blog

--- controller/api/userApiRoutes.js
+++ controller/api/userApiRoutes.js
@@ -1,9 +1,46 @@
 // create router object
 const router = require('express').Router();
+const User = require('../../model/User');
 
-router.post('/login', (req, res) => {
-    res.json({ message: 'test' });
-})
+router.post('/login', async (req, res) => {
+    try {
+        // Find a user in our database that matches the email
+        const userData = User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
+
+        // if a user was NOT found, return an error
+        if (!userData) {
+            res
+                .status(404)
+                .json({ message: "Incorrect Email!" });
+        }
+
+        // Check if the password supplied in the body matches the user's password
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+                .status(404)
+                .json({ message: "Incorrect Password" });
+            return;
+        }
+
+        // Set loggedIn to true on the session
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            res.session.username = userData.username;
+            res.session.loggedIn = true;
+
+            res.json({ user: userData, message: "Log In Successful!" });
+        })
+
+    } catch (err) {
+        res.status(404).json(err);
+    }
+});
 
 /*
 43:57 in lecture video
--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -2,7 +2,7 @@ function checkAuth(req, res, next) {
     if (!req.session.loggedIn) {
         res.redirect('/login');
     } else {
-        next();
+        return next();
     }
 }
 
